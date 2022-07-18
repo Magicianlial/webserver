00d9ea8a12bb1bbee8605be27907dc183fd1d057
alyesc <guopengjun1998@163.com>
2022-07-18 20:36:34 +0800

    threadpoll

--- tcp_test/threadpool/threadpoll.h
+++ tcp_test/threadpool/threadpoll.h
@@ -1,10 +1,24 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
+#include <pthread.h>
+
 // 线程池类，定义成模板类（代码复用）
 template<typename T>
 class threadpool {
-    
+public:
+
+private:
+    // 线程数量
+    int m_thread_number;
+
+    // 线程池数组
+    pthread_t *m_threads;
+
+    // 请求队列中最大允许等待处理请求数量
+    int m_max_requests;
+
+    //请求队列
 }
 
 
